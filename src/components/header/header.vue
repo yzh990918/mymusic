c60ecc568aa11da4bdeb1f7304898b418df4eb89
251205668@qq.com <251205668@qq.com>
2019-11-09 17:39:36 +0800

    修改配置

---
+++ src/components/header/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'header',
+  props: [''],
+  data () {
+    return {
+
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount () { },
+
+  mounted () { },
+
+  methods: {},
+
+  watch: {}
+
+}
+
+</script>
+<style lang='stylus' scoped></style>
