e58228152676a895778a3115976960ce7caff396
251205668@qq.com <251205668@qq.com>
2019-11-09 16:39:44 +0800

    初始化项目

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+import store from './store/index'
+import './common/stylus/index.styl'
+Vue.config.productionTip = false
+Vue.use(VueLazyload, {
+  loading: require('./common/image/default.png')
+})
+fastclick.attach(document.body)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: {
+    App
+  },
+  template: '<App/>'
+})
