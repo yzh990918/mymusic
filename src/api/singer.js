7268825dd88ef281cb13aaaa06f3672c43d2daf4
251205668@qq.com <251205668@qq.com>
2019-11-19 18:11:20 +0800

    获取Singerlist数据 封装

---
+++ src/api/singer.js
@@ -0,0 +1,20 @@
+import jsonp from '../common/js/jsonp'
+import {
+  commonParams,
+  options
+} from './config.js'
+export function getSingerList () {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    hostUin: 0,
+    needNewcode: 0,
+    platform: 'yqq',
+    g_tk: 1664028744
+  })
+  return jsonp(url, data, options)
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
-    歌手页面
+  <div class="singer">
+
   </div>
 </template>
 
 <script>
-
+import { getSingerList } from '../../api/singer'
+import { ERR_OK } from '../../api//config'
+import Singer from '../../common/js/singer'
 export default {
   name: 'singer',
   props: [''],
   data () {
     return {
+      singers: []
 
     }
   },
@@ -20,14 +23,66 @@ export default {
   computed: {},
 
   beforeMount () { },
-
+  created () {
+    this._getsingerList()
+  },
   mounted () { },
 
-  methods: {},
+  methods: {
+    _getsingerList () {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+          console.log(this._normallizeSinger(this.singers))
+        }
+      })
+    },
+    _normallizeSinger (list) {
+      let map = {
+        hot: {
+          title: '热门',
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < 10) {
+          map.hot.items.push(new Singer({
+            // todo 热门区歌手对象
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        // * 歌手区list
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            // todo map[key]：A.B.C...区块 如果没有key就生成一个list对象 往每个map[key]里面push Singer对象
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(
+          new Singer({
+            // todo 热门区歌手对象
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          })
+        )
+      })
+      return map
+    }
+  },
 
   watch: {}
 
 }
 
 </script>
-<style lang='stylus' scoped></style>
+<style lang='stylus' scoped>
+@import '~@/common/stylus/variable.styl'
+.singer
+  position: fixed
+  top: 88px
+  bottom: 0
+  width: 100%
+</style>
