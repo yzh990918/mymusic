73fc1c2e1814b1e41ffe0b0a97a9ee45bf8cde48
251205668@qq.com <251205668@qq.com>
2019-12-22 16:48:38 +0800

    抓取数据

--- src/api/singer.js
+++ src/api/singer.js
@@ -1,20 +1,32 @@
-import jsonp from '../common/js/jsonp'
-import {
-  commonParams,
-  options
-} from './config.js'
-export function getSingerList () {
-  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+import jsonp from "../common/js/jsonp";
+import { commonParams, options } from "./config.js";
+export function getSingerList() {
+  const url = "https://c.y.qq.com/v8/fcg-bin/v8.fcg";
   const data = Object.assign({}, commonParams, {
-    channel: 'singer',
-    page: 'list',
-    key: 'all_all_all',
+    channel: "singer",
+    page: "list",
+    key: "all_all_all",
     pagesize: 100,
     pagenum: 1,
     hostUin: 0,
     needNewcode: 0,
-    platform: 'yqq',
+    platform: "yqq",
     g_tk: 1664028744
-  })
-  return jsonp(url, data, options)
+  });
+  return jsonp(url, data, options);
+}
+export function getSingerDetail(singerId) {
+  const url = "https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg";
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    needNewCode: 0,
+    platform: "yqq",
+    order: "listen",
+    begin: 0,
+    num: 200,
+    songstatus: 1,
+    singermid: singerId,
+    g_tk: 1664028744
+  });
+  return jsonp(url, data, options);
 }
